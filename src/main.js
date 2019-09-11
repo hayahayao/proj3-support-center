aaa920b839083335a31f76f1a5c4d21c15a1977f
hayahayao <azurecho13@gmail.com>
2019-09-11 15:33:49 +0800

    base structure

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
+
+import AppLayout from './components/AppLayout.vue';
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  render: h => h(AppLayout),
   router,
-  render: h => h(App)
-}).$mount('#app')
+})
--- src/router.js
+++ src/router.js
@@ -1,23 +1,19 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+import Home from './components/Home.vue'
+import FAQ from './components/FAQ.vue'
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/', name: 'home', component: Home },
+    { path: '/faq', name: 'faq', component: FAQ },
+]
+
+const router = new VueRouter({
+    routes,
+    mode: 'history',
 })
+
+export default router
