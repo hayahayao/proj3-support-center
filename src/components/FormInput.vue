188ad47058117f8ca385c88ff10403bf354c5d44
hayahayao <azurecho13@gmail.com>
2019-09-17 18:05:06 +0800

    login: components

---
+++ src/components/FormInput.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="row">
+        <input
+            class="input"
+            :class="inputClass"
+            :name="name"
+            :type="type"
+            :value="text"
+            :placeholder="placeholder"
+            @input="update"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+        },
+        type: {
+            type: String,
+            default: 'text',
+        },
+        text: {
+            required: true,
+        },
+        placeholder: {
+            type: String,
+        },
+        invalid: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    computed: {
+        inputClass() {
+            return {
+                'invalid': this.invalid,
+            }
+        },
+    },
+    methods: {
+        update(event) {
+            this.$emit('update', event.target.value)
+        }
+    },
+    model: {
+        prop: 'text',
+        event: 'update',
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import Vue from 'vue'
 import router from './router'
+import state from './state'
 
 import AppLayout from './components/AppLayout.vue';
 import './global-components'
 
 import VueFetch from './plugins/fetch'
+import VueState from './plugins/state'
 
 Vue.use(VueFetch, {
   baseUrl: 'http://localhost:3000/'
 })
 
+Vue.use(VueState, state)
+
 new Vue({
   el: '#app',
-  render: h => h(AppLayout),
+  data: state,
   router,
+  render: h => h(AppLayout),
 })
